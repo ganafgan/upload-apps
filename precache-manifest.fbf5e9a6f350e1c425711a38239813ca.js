523ddaa3d9c4b47f7a5acce0ff19f013efddada6
afgantaufiq <ganafgan@gmail.com>
2020-04-21 16:58:08 +0700

    upload

---
+++ precache-manifest.fbf5e9a6f350e1c425711a38239813ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5983f4976b46693ee92569f392b8f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba809af2e2e1f2d1c9bf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ba809af2e2e1f2d1c9bf",
+    "url": "/static/js/2.9954df54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9954df54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6400b8c1412762e5417b",
+    "url": "/static/js/main.39a5b476.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
